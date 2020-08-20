f7f64aa9cac07d68ee5b7357700003b4bd7d3742
Allie <williamson.alisonj@gmail.com>
2020-08-20 02:20:54 -0400

    array of questions for each employee type

--- Develop/app.js
+++ Develop/app.js
@@ -10,62 +10,87 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
-const questions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your id?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your email?"
-    },
-    {
-        type: "input",
-        name: "officeNumber",
-        message: "What is your office number?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is your email?"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is your Github username?"
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What school do you go to?"
-    },
+const teamMembers = [];
+
+// choose what type of employee you would like to add
+const employeeType = [
+    { 
+        type: "list", 
+        name: "employeeType", 
+        message: "What type of employee would you like to add?", 
+        choices: ["Manager", "Engineer", "Intern", "None"] 
+    }
 ]
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// array of questions about the manager
+const managerQuestions = [
+    { 
+        type: "input", 
+        name: "name", 
+        message: "What is the manager's name?" 
+    },  
+    { 
+        type: "input", 
+        name: "id", 
+        message: "What is the manager's ID?" 
+    },
+    { 
+        type: "input", 
+        name: "email", 
+        message: "What is the manager's email?"
+    }, 
+    { 
+        type:"input", 
+        name: "officeNumber", 
+        message: "What is the manager's office number?"
+    }
+]
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// array of questions about the engineer
+const engineerQuestions = [
+    { 
+        type: "input", 
+        name: "name", 
+        message: "What is the engineer's name?" 
+    },  
+    { 
+        type: "input", 
+        name: "id", 
+        message: "What is the engineer's ID?" 
+    },
+    { 
+        type: "input", 
+        name: "email", 
+        message: "What is the engineer's email?" 
+    }, 
+    { 
+        type: "input", 
+        name: "github", 
+        message: "What is the engineer's GitHub username?" 
+    }
+]
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+// array of questions about the intern
+const internQuestions = [
+    { 
+        type: "input", 
+        name: "name", 
+        message: "What is the intern's name?" 
+    }, 
+    { 
+        type: "input", 
+        name: "id", 
+        message: "What is the intern's ID?" 
+    },
+    { 
+        type: "input", 
+        name: "email", 
+        message: "What is the intern's email?" 
+    },  
+    { 
+        type: "input", 
+        name: "schoolName", 
+        message: "What school does the intern attend?" 
+    }
+]
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
