408143997b309cae66bd89bdbf969ccfab0b538b
Allie <williamson.alisonj@gmail.com>
2020-08-20 03:22:39 -0400

    bug fix

--- Develop/lib/Manager.js
+++ Develop/lib/Manager.js
@@ -1,7 +1,6 @@
-// TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 const Employee = require("./Employee.js");
 
-// not sure if right yet..
+// defines and exports the manager class
 class Manager extends Employee {
     constructor (name, id, email, officeNumber) {
         super (name, id, email)
